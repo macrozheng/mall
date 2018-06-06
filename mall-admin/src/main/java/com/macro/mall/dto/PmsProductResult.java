2b0372f46d394216c378faeaff64586f6bb82d6c
zhh <zh@thinkerx.com>
2018-06-06 17:24:38 +0800

    商品接口修改

--- mall-admin/src/main/java/com/macro/mall/dto/PmsProductResult.java
+++ mall-admin/src/main/java/com/macro/mall/dto/PmsProductResult.java
@@ -1,44 +1,12 @@
 package com.macro.mall.dto;
 
-import com.macro.mall.model.CmsPrefrenceArea;
-import com.macro.mall.model.CmsSubject;
-
-import java.util.List;
-
 /**
  * 查询单个产品进行修改时返回的结果
  * Created by macro on 2018/4/26.
  */
-public class PmsProductResult extends PmsProductParam{
+public class PmsProductResult extends PmsProductParam {
     //商品所选分类的父id
     private Long cateParentId;
-    private String productAttributeCategoryName;
-    private List<CmsSubject> subjectList;
-    private List<CmsPrefrenceArea> prefrenceAreaList;
-
-    public String getProductAttributeCategoryName() {
-        return productAttributeCategoryName;
-    }
-
-    public void setProductAttributeCategoryName(String productAttributeCategoryName) {
-        this.productAttributeCategoryName = productAttributeCategoryName;
-    }
-
-    public List<CmsSubject> getSubjectList() {
-        return subjectList;
-    }
-
-    public void setSubjectList(List<CmsSubject> subjectList) {
-        this.subjectList = subjectList;
-    }
-
-    public List<CmsPrefrenceArea> getPrefrenceAreaList() {
-        return prefrenceAreaList;
-    }
-
-    public void setPrefrenceAreaList(List<CmsPrefrenceArea> prefrenceAreaList) {
-        this.prefrenceAreaList = prefrenceAreaList;
-    }
 
     public Long getCateParentId() {
         return cateParentId;
