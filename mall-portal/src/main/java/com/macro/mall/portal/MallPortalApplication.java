983293ff48767249ef41083afb0bf5b4d7dc38db
zhh <zh@thinkerx.com>
2018-08-01 13:20:34 +0800

    添加前台工程

---
+++ mall-portal/src/main/java/com/macro/mall/portal/MallPortalApplication.java
@@ -0,0 +1,14 @@
+package com.macro.mall.portal;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan({"com.macro.mall.mapper","com.macro.mall.portal.dao"})
+public class MallPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallPortalApplication.class, args);
+    }
+}
---
+++ mall-portal/src/test/java/com/macro/mall/portal/MallPortalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.macro.mall.portal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MallPortalApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
