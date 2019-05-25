2a520725def805a05eaefc44fcb4e40797df1219
macro <zhenghong1991@126.com>
2019-05-25 15:22:27 +0800

    oss相关代码改写

--- mall-admin/src/main/java/com/macro/mall/dto/OssCallbackParam.java
+++ mall-admin/src/main/java/com/macro/mall/dto/OssCallbackParam.java
@@ -1,11 +1,17 @@
 package com.macro.mall.dto;
 
+import io.swagger.annotations.ApiModelProperty;
+
 /**
+ * oss上传成功后的回调参数
  * Created by macro on 2018/5/17.
  */
 public class OssCallbackParam {
+    @ApiModelProperty("请求的回调地址")
     private String callbackUrl;
+    @ApiModelProperty("回调是传入request中的参数")
     private String callbackBody;
+    @ApiModelProperty("回调时传入参数的格式，比如表单提交形式")
     private String callbackBodyType;
 
     public String getCallbackUrl() {
--- mall-admin/src/main/java/com/macro/mall/dto/OssPolicyResult.java
+++ mall-admin/src/main/java/com/macro/mall/dto/OssPolicyResult.java
@@ -1,15 +1,24 @@
 package com.macro.mall.dto;
 
+import io.swagger.annotations.ApiModelProperty;
+
 /**
- * 获取OSS上传授权返回结果
+ * 获取OSS上传文件授权返回结果
  * Created by macro on 2018/5/17.
  */
 public class OssPolicyResult {
+    @ApiModelProperty("访问身份验证中用到用户标识")
     private String accessKeyId;
+    @ApiModelProperty("用户表单上传的策略,经过base64编码过的字符串")
     private String policy;
+    @ApiModelProperty("对policy签名后的字符串")
     private String signature;
+    @ApiModelProperty("上传文件夹路径前缀")
     private String dir;
+    @ApiModelProperty("oss对外服务的访问域名")
     private String host;
+    @ApiModelProperty("上传成功后的回调设置")
+    private String callback;
 
     public String getAccessKeyId() {
         return accessKeyId;
@@ -49,5 +58,13 @@ public class OssPolicyResult {
 
     public void setHost(String host) {
         this.host = host;
+    }
+
+    public String getCallback() {
+        return callback;
+    }
+
+    public void setCallback(String callback) {
+        this.callback = callback;
     }
 }
