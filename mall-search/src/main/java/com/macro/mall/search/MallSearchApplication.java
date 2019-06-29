dd33e9be3feb1173981ee1ee3f45721b54c143b1
Diino <roragnarok@qq.com>
2019-06-29 23:58:50 +0800

    增加对spring cloud的支持

--- mall-search/src/main/java/com/macro/mall/search/MallSearchApplication.java
+++ mall-search/src/main/java/com/macro/mall/search/MallSearchApplication.java
@@ -2,8 +2,10 @@ package com.macro.mall.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MallSearchApplication {
 
     public static void main(String[] args) {
