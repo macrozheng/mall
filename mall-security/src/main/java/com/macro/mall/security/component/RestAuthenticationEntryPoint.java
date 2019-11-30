21dba8d15719cbcb9ac12326422fc58efe442fa9
macro <zhenghong1991@126.com>
2019-11-30 20:16:43 +0800

    mall-security注释修改

--- mall-security/src/main/java/com/macro/mall/security/component/RestAuthenticationEntryPoint.java
+++ mall-security/src/main/java/com/macro/mall/security/component/RestAuthenticationEntryPoint.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * 当未登录或者token失效访问接口时，自定义的返回结果
+ * 自定义返回结果：未登录或登录过期
  * Created by macro on 2018/5/14.
  */
 public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
