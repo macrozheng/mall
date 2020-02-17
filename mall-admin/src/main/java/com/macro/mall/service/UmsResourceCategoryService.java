98ad1506478ca5f69efb747f54e4afbf3338748c
macro <zhenghong1991@126.com>
2020-02-17 21:40:00 +0800

    Update UmsResourceCategoryService.java

--- mall-admin/src/main/java/com/macro/mall/service/UmsResourceCategoryService.java
+++ mall-admin/src/main/java/com/macro/mall/service/UmsResourceCategoryService.java
@@ -9,11 +9,24 @@ import java.util.List;
  * Created by macro on 2020/2/5.
  */
 public interface UmsResourceCategoryService {
+
+    /**
+     * 获取所有资源分类
+     */
     List<UmsResourceCategory> listAll();
 
+    /**
+     * 创建资源分类
+     */
     int create(UmsResourceCategory umsResourceCategory);
 
+    /**
+     * 修改资源分类
+     */
     int update(Long id, UmsResourceCategory umsResourceCategory);
 
+    /**
+     * 删除资源分类
+     */
     int delete(Long id);
 }
