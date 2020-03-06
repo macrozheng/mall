94d44c29d0b139c32426c6fcc0a06086c0d63534
macro <zhenghong1991@126.com>
2020-03-06 20:37:06 +0800

    Update PmsProductAttributeService.java

--- mall-admin/src/main/java/com/macro/mall/service/PmsProductAttributeService.java
+++ mall-admin/src/main/java/com/macro/mall/service/PmsProductAttributeService.java
@@ -14,7 +14,8 @@ import java.util.List;
 public interface PmsProductAttributeService {
     /**
      * 根据分类分页获取商品属性
-     * @param cid 分类id
+     *
+     * @param cid  分类id
      * @param type 0->属性；2->参数
      */
     List<PmsProductAttribute> getList(Long cid, Integer type, Integer pageSize, Integer pageNum);
@@ -35,8 +36,14 @@ public interface PmsProductAttributeService {
      */
     PmsProductAttribute getItem(Long id);
 
+    /**
+     * 批量删除商品属性
+     */
     @Transactional
     int delete(List<Long> ids);
 
+    /**
+     * 获取和分类相关的商品属性
+     */
     List<ProductAttrInfo> getProductAttrInfo(Long productCategoryId);
 }
