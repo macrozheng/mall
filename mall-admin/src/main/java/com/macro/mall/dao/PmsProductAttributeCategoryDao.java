2d0fb84a8e383a6c5531b3defcac631fb927c1f7
macro <zhenghong1991@126.com>
2020-03-08 20:55:58 +0800

    Update PmsProductAttributeCategoryDao.java

--- mall-admin/src/main/java/com/macro/mall/dao/PmsProductAttributeCategoryDao.java
+++ mall-admin/src/main/java/com/macro/mall/dao/PmsProductAttributeCategoryDao.java
@@ -9,5 +9,8 @@ import java.util.List;
  * Created by macro on 2018/5/24.
  */
 public interface PmsProductAttributeCategoryDao {
+    /**
+     * 获取包含商品属性的商品属性分类列表
+     */
     List<PmsProductAttributeCategoryItem> getListWithAttr();
 }
