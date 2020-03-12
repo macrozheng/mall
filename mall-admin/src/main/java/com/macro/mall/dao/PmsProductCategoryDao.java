0a1c388280f493ba33733dcf310c2cd8bbb784fa
macro <zhenghong1991@126.com>
2020-03-12 18:50:03 +0800

    Update PmsProductCategoryDao.java

--- mall-admin/src/main/java/com/macro/mall/dao/PmsProductCategoryDao.java
+++ mall-admin/src/main/java/com/macro/mall/dao/PmsProductCategoryDao.java
@@ -9,5 +9,8 @@ import java.util.List;
  * Created by macro on 2018/5/25.
  */
 public interface PmsProductCategoryDao {
+    /**
+     * 获取包括子分类的商品分类列表
+     */
     List<PmsProductCategoryWithChildrenItem> listWithChildren();
 }
