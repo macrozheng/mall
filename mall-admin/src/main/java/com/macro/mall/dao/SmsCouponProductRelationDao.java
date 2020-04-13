8e80cde7f8c1973a353a85f24a02d3d7b10f561a
macro <zhenghong1991@126.com>
2020-04-13 20:17:37 +0800

    Update SmsCouponProductRelationDao.java

--- mall-admin/src/main/java/com/macro/mall/dao/SmsCouponProductRelationDao.java
+++ mall-admin/src/main/java/com/macro/mall/dao/SmsCouponProductRelationDao.java
@@ -10,5 +10,8 @@ import java.util.List;
  * Created by macro on 2018/8/28.
  */
 public interface SmsCouponProductRelationDao {
+    /**
+     * 批量创建
+     */
     int insertList(@Param("list")List<SmsCouponProductRelation> productRelationList);
 }
