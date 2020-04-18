9b81ad74b3ac92d8509d35609130a4885f4c71ac
macro <zhenghong1991@126.com>
2020-04-18 14:22:57 +0800

    Update UmsRoleDao.java

--- mall-admin/src/main/java/com/macro/mall/dao/UmsRoleDao.java
+++ mall-admin/src/main/java/com/macro/mall/dao/UmsRoleDao.java
@@ -19,6 +19,8 @@ public interface UmsRoleDao {
      * 根据角色ID获取菜单
      */
     List<UmsMenu> getMenuListByRoleId(@Param("roleId") Long roleId);
-
+    /**
+     * 根据角色ID获取资源
+     */
     List<UmsResource> getResourceListByRoleId(@Param("roleId") Long roleId);
 }
