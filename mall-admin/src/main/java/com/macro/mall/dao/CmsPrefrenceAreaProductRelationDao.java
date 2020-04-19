6ed19129645c534bd6028adca902ecdbc3709f9a
macro <zhenghong1991@126.com>
2020-04-19 17:04:22 +0800

    Update CmsPrefrenceAreaProductRelationDao.java

--- mall-admin/src/main/java/com/macro/mall/dao/CmsPrefrenceAreaProductRelationDao.java
+++ mall-admin/src/main/java/com/macro/mall/dao/CmsPrefrenceAreaProductRelationDao.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 /**
- * 自定义优选和商品关系操作
+ * 自定义优选和商品关系操作Dao
  * Created by macro on 2018/4/26.
  */
 public interface CmsPrefrenceAreaProductRelationDao {
