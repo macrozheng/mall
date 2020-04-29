ebf35c0a27f7a27cd831ee01625ae37d9ab3d589
macro <zhenghong1991@126.com>
2020-04-29 20:43:10 +0800

    Update UmsAdminPermissionRelationDao.java

--- mall-admin/src/main/java/com/macro/mall/dao/UmsAdminPermissionRelationDao.java
+++ mall-admin/src/main/java/com/macro/mall/dao/UmsAdminPermissionRelationDao.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 /**
- * 用户权限自定义Dao
+ * 自定义用户权限关系管理Dao
  * Created by macro on 2018/10/8.
  */
 public interface UmsAdminPermissionRelationDao {
