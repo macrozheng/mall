a51c42cca421852284c8c065f0c9ec95e2d40b2e
macro <zhenghong1991@126.com>
2020-05-01 16:58:21 +0800

    Update UmsAdminRoleRelationDao.java

--- mall-admin/src/main/java/com/macro/mall/dao/UmsAdminRoleRelationDao.java
+++ mall-admin/src/main/java/com/macro/mall/dao/UmsAdminRoleRelationDao.java
@@ -9,7 +9,7 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 /**
- * 后台用户与角色管理自定义Dao
+ * 自定义后台用户与角色管理Dao
  * Created by macro on 2018/10/8.
  */
 public interface UmsAdminRoleRelationDao {
