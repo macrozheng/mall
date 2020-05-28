4c56de45060237b0bb4e85d3001f24734737e238
macro <zhenghong1991@126.com>
2020-05-28 21:56:00 +0800

    Update PmsProductAttributeCategoryItem.java

--- mall-admin/src/main/java/com/macro/mall/dto/PmsProductAttributeCategoryItem.java
+++ mall-admin/src/main/java/com/macro/mall/dto/PmsProductAttributeCategoryItem.java
@@ -3,6 +3,9 @@ package com.macro.mall.dto;
 import com.macro.mall.model.PmsProductAttribute;
 import com.macro.mall.model.PmsProductAttributeCategory;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
@@ -11,14 +14,8 @@ import java.util.List;
  * Created by macro on 2018/5/24.
  */
 public class PmsProductAttributeCategoryItem extends PmsProductAttributeCategory {
+    @Getter
+    @Setter
     @ApiModelProperty(value = "商品属性列表")
     private List<PmsProductAttribute> productAttributeList;
-
-    public List<PmsProductAttribute> getProductAttributeList() {
-        return productAttributeList;
-    }
-
-    public void setProductAttributeList(List<PmsProductAttribute> productAttributeList) {
-        this.productAttributeList = productAttributeList;
-    }
 }
