8e33027520c554b7363c3859cd6db8dd2585ef89
macro <zhenghong1991@126.com>
2020-06-22 20:42:10 +0800

    Update OssConfig.java

--- mall-admin/src/main/java/com/macro/mall/config/OssConfig.java
+++ mall-admin/src/main/java/com/macro/mall/config/OssConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * OSS对象存储相关配置
  * Created by macro on 2018/5/17.
  */
 @Configuration
