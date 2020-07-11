5ac40a5036adfa2dea93f6aa7a7e8ba4f00bbe2b
macro <zhenghong1991@126.com>
2020-07-11 10:50:32 +0800

    Swagger 版本升级

--- mall-admin/src/main/java/com/macro/mall/config/Swagger2Config.java
+++ mall-admin/src/main/java/com/macro/mall/config/Swagger2Config.java
@@ -56,9 +56,7 @@ public class Swagger2Config {
     private List<SecurityContext> securityContexts() {
         //设置需要登录认证的路径
         List<SecurityContext> result = new ArrayList<>();
-        result.add(getContextByPath("/brand/.*"));
-        result.add(getContextByPath("/product/.*"));
-        result.add(getContextByPath("/productCategory/.*"));
+        result.add(getContextByPath("/*/.*"));
         return result;
     }
 
