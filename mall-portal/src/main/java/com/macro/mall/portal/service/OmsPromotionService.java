0f20132e328faa5d97f55a9634389f41d21227a1
macro <zhenghong1991@126.com>
2020-07-12 20:29:40 +0800

    Update OmsPromotionService.java

--- mall-portal/src/main/java/com/macro/mall/portal/service/OmsPromotionService.java
+++ mall-portal/src/main/java/com/macro/mall/portal/service/OmsPromotionService.java
@@ -6,8 +6,8 @@ import com.macro.mall.portal.domain.CartPromotionItem;
 import java.util.List;
 
 /**
- * Created by macro on 2018/8/27.
  * 促销管理Service
+ * Created by macro on 2018/8/27.
  */
 public interface OmsPromotionService {
     /**
