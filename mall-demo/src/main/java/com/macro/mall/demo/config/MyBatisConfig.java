77b08879746f3dd7559f305691d7e56814e177e6
macro <zhenghong1991@126.com>
2020-08-02 19:57:42 +0800

    Update MyBatisConfig.java

--- mall-demo/src/main/java/com/macro/mall/demo/config/MyBatisConfig.java
+++ mall-demo/src/main/java/com/macro/mall/demo/config/MyBatisConfig.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * MyBatis配置类
+ * MyBatis相关配置
  * Created by macro on 2019/4/8.
  */
 @Configuration
