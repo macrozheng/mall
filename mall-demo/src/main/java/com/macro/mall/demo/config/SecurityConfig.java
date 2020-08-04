934883648bb982c4220fb891360d6cae9cef8a49
macro <zhenghong1991@126.com>
2020-08-04 20:52:44 +0800

    Update SecurityConfig.java

--- mall-demo/src/main/java/com/macro/mall/demo/config/SecurityConfig.java
+++ mall-demo/src/main/java/com/macro/mall/demo/config/SecurityConfig.java
@@ -31,7 +31,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()//配置权限
 //                .antMatchers("/").access("hasRole('TEST')")//该路径需要TEST角色
-                .antMatchers("/").authenticated()//该路径需要登录认证
 //                .antMatchers("/brand/list").hasAuthority("TEST")//该路径需要TEST权限
                 .antMatchers("/**").permitAll()
                 .and()//启用基于http的认证
