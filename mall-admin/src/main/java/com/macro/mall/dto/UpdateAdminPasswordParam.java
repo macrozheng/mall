343ebe13cb8825f27ca9c99caa2a5958f0ca806d
macro <zhenghong1991@126.com>
2020-09-03 20:53:21 +0800

    Update UpdateAdminPasswordParam.java

--- mall-admin/src/main/java/com/macro/mall/dto/UpdateAdminPasswordParam.java
+++ mall-admin/src/main/java/com/macro/mall/dto/UpdateAdminPasswordParam.java
@@ -13,13 +13,13 @@ import javax.validation.constraints.NotEmpty;
 @Getter
 @Setter
 public class UpdateAdminPasswordParam {
+    @NotEmpty
     @ApiModelProperty(value = "用户名", required = true)
-    @NotEmpty(message = "用户名不能为空")
     private String username;
+    @NotEmpty
     @ApiModelProperty(value = "旧密码", required = true)
-    @NotEmpty(message = "旧密码不能为空")
     private String oldPassword;
+    @NotEmpty
     @ApiModelProperty(value = "新密码", required = true)
-    @NotEmpty(message = "新密码不能为空")
     private String newPassword;
 }
