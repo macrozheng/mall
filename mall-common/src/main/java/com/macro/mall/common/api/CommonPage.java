8c15f7d88f30bfd19aa81e2f04327b9e8b73ff1d
macro <zhenghong1991@126.com>
2020-09-07 20:59:18 +0800

    Update CommonPage.java

--- mall-common/src/main/java/com/macro/mall/common/api/CommonPage.java
+++ mall-common/src/main/java/com/macro/mall/common/api/CommonPage.java
@@ -10,10 +10,25 @@ import java.util.List;
  * Created by macro on 2019/4/19.
  */
 public class CommonPage<T> {
+    /**
+     * 当前页码
+     */
     private Integer pageNum;
+    /**
+     * 每页数量
+     */
     private Integer pageSize;
+    /**
+     * 总页数
+     */
     private Integer totalPage;
+    /**
+     * 总条数
+     */
     private Long total;
+    /**
+     * 分页数据
+     */
     private List<T> list;
 
     /**
