8ef0b9ef1faae6c9a09239def939f8f9a5f4f8e0
macro <zhenghong1991@126.com>
2020-10-29 20:52:44 +0800

    Update mall-search.sh

--- document/sh/mall-search.sh
+++ document/sh/mall-search.sh
@@ -9,6 +9,7 @@ echo '----rm none images----'
 docker run -p 8081:8081 --name ${app_name} \
 --link mysql:db \
 --link elasticsearch:es \
+-e TZ="Asia/Shanghai" \
 -v /etc/localtime:/etc/localtime \
 -v /mydata/app/${app_name}/logs:/var/logs \
 -d mall/${app_name}:1.0-SNAPSHOT
