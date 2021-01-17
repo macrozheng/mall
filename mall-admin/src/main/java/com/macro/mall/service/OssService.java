8439170d45d8ce39d39ba82bec75604b33416de9
macro <zhenghong1991@126.com>
2021-01-17 15:27:46 +0800

    Update OssService.java

--- mall-admin/src/main/java/com/macro/mall/service/OssService.java
+++ mall-admin/src/main/java/com/macro/mall/service/OssService.java
@@ -11,11 +11,11 @@ import javax.servlet.http.HttpServletRequest;
  */
 public interface OssService {
     /**
-     * oss上传策略生成
+     * Oss上传策略生成
      */
     OssPolicyResult policy();
     /**
-     * oss上传成功回调
+     * Oss上传成功回调
      */
     OssCallbackResult callback(HttpServletRequest request);
 }
