824a2ba29c6c1c6cc065c6321cd4e329f03d869a
macro <zhenghong1991@126.com>
2021-01-28 20:57:28 +0800

    Update SmsFlashPromotionProductRelationService.java

--- mall-admin/src/main/java/com/macro/mall/service/SmsFlashPromotionProductRelationService.java
+++ mall-admin/src/main/java/com/macro/mall/service/SmsFlashPromotionProductRelationService.java
@@ -42,9 +42,8 @@ public interface SmsFlashPromotionProductRelationService {
 
     /**
      * 根据活动和场次id获取商品关系数量
-     * @param flashPromotionId
-     * @param flashPromotionSessionId
-     * @return
+     * @param flashPromotionId        限时购id
+     * @param flashPromotionSessionId 限时购场次id
      */
     long getCount(Long flashPromotionId,Long flashPromotionSessionId);
 }
