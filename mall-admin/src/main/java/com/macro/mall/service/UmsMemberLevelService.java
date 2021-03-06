cc652a5c079b102cfbbff89bc22b2f557eb355e9
macro <zhenghong1991@126.com>
2021-03-06 19:58:59 +0800

    Update UmsMemberLevelService.java

--- mall-admin/src/main/java/com/macro/mall/service/UmsMemberLevelService.java
+++ mall-admin/src/main/java/com/macro/mall/service/UmsMemberLevelService.java
@@ -10,7 +10,7 @@ import java.util.List;
  */
 public interface UmsMemberLevelService {
     /**
-     * 获取所有会员登录
+     * 获取所有会员等级
      * @param defaultStatus 是否为默认会员
      */
     List<UmsMemberLevel> list(Integer defaultStatus);
