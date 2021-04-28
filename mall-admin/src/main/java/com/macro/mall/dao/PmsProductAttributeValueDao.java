14f0a625bfa421dbf9d5e7f357c9dc0c6ac03f86
macro <zhenghong1991@126.com>
2021-04-28 21:23:42 +0800

    Update PmsProductAttributeValueDao.java

--- mall-admin/src/main/java/com/macro/mall/dao/PmsProductAttributeValueDao.java
+++ mall-admin/src/main/java/com/macro/mall/dao/PmsProductAttributeValueDao.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 /**
- * 商品参数，商品自定义规格属性Dao
+ * 商品参数/规格属性自定义Dao
  * Created by macro on 2018/4/26.
  */
 public interface PmsProductAttributeValueDao {
