217317dd1b9146b3209d196d64e13de71c13fce0
macro <zhenghong1991@126.com>
2021-05-05 16:44:15 +0800

    Update PmsProductFullReductionDao.java

--- mall-admin/src/main/java/com/macro/mall/dao/PmsProductFullReductionDao.java
+++ mall-admin/src/main/java/com/macro/mall/dao/PmsProductFullReductionDao.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 /**
- * 自定义商品满减Dao
+ * 商品满减自定义Dao
  * Created by macro on 2018/4/26.
  */
 public interface PmsProductFullReductionDao {
