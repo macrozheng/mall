79b5cbca0a4094054e6bc59174b1c421decb2785
macro <zhenghong1991@126.com>
2021-05-11 20:29:21 +0800

    Update PmsSkuStockDao.java

--- mall-admin/src/main/java/com/macro/mall/dao/PmsSkuStockDao.java
+++ mall-admin/src/main/java/com/macro/mall/dao/PmsSkuStockDao.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 /**
- * 自定义商品SKU管理Dao
+ * 商品SKU管理自定义Dao
  * Created by macro on 2018/4/26.
  */
 public interface PmsSkuStockDao {
