8c1942c2dd312d5b122130fcc425406477d86881
macro <zhenghong1991@126.com>
2021-05-13 20:49:28 +0800

    Update SmsCouponDao.java

--- mall-admin/src/main/java/com/macro/mall/dao/SmsCouponDao.java
+++ mall-admin/src/main/java/com/macro/mall/dao/SmsCouponDao.java
@@ -4,7 +4,7 @@ import com.macro.mall.dto.SmsCouponParam;
 import org.apache.ibatis.annotations.Param;
 
 /**
- * 自定义优惠券管理Dao
+ * 优惠券管理自定义Dao
  * Created by macro on 2018/8/29.
  */
 public interface SmsCouponDao {
