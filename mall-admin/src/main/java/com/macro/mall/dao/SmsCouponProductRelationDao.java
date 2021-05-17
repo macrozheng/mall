42c10fcaff2b548da982dcdb7c054a782d4ce0d2
macro <zhenghong1991@126.com>
2021-05-17 20:30:31 +0800

    Update SmsCouponProductRelationDao.java

--- mall-admin/src/main/java/com/macro/mall/dao/SmsCouponProductRelationDao.java
+++ mall-admin/src/main/java/com/macro/mall/dao/SmsCouponProductRelationDao.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 /**
- * 自定义优惠券和商品关系关系Dao
+ * 优惠券和商品关系自定义Dao
  * Created by macro on 2018/8/28.
  */
 public interface SmsCouponProductRelationDao {
