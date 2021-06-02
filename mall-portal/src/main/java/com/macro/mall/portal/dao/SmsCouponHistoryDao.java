73207300e76a4b76ca846a0acf013bbb2915e0dd
macro <zhenghong1991@126.com>
2021-06-02 20:29:01 +0800

    Update SmsCouponHistoryDao.java

--- mall-portal/src/main/java/com/macro/mall/portal/dao/SmsCouponHistoryDao.java
+++ mall-portal/src/main/java/com/macro/mall/portal/dao/SmsCouponHistoryDao.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 /**
- * 会员优惠券领取历史自定义Dao
+ * 会员优惠券领取记录自定义Dao
  * Created by macro on 2018/8/29.
  */
 public interface SmsCouponHistoryDao {
