1efae9f707e0eb261a0a523dbb0ecd1bd4d89b5a
macro <zhenghong1991@126.com>
2021-06-23 20:25:22 +0800

    Update OrderTimeOutCancelTask.java

--- mall-portal/src/main/java/com/macro/mall/portal/component/OrderTimeOutCancelTask.java
+++ mall-portal/src/main/java/com/macro/mall/portal/component/OrderTimeOutCancelTask.java
@@ -8,8 +8,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
- * Created by macro on 2018/8/24.
  * 订单超时取消并解锁库存的定时器
+ * Created by macro on 2018/8/24.
  */
 @Component
 public class OrderTimeOutCancelTask {
