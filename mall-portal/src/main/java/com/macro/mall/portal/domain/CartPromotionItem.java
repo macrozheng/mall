1db4ee8ea2eaf8f9809477c4c3405e3d443522a5
macro <zhenghong1991@126.com>
2021-07-19 20:59:45 +0800

    Update CartPromotionItem.java

--- mall-portal/src/main/java/com/macro/mall/portal/domain/CartPromotionItem.java
+++ mall-portal/src/main/java/com/macro/mall/portal/domain/CartPromotionItem.java
@@ -5,8 +5,8 @@ import com.macro.mall.model.OmsCartItem;
 import java.math.BigDecimal;
 
 /**
- * Created by macro on 2018/8/27.
  * 购物车中促销信息的封装
+ * Created by macro on 2018/8/27.
  */
 public class CartPromotionItem extends OmsCartItem{
     //促销活动信息
