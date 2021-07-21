be0720cf6dad860af44c9d6596712eb8b7996dd9
macro <zhenghong1991@126.com>
2021-07-21 20:39:08 +0800

    Update CartProduct.java

--- mall-portal/src/main/java/com/macro/mall/portal/domain/CartProduct.java
+++ mall-portal/src/main/java/com/macro/mall/portal/domain/CartProduct.java
@@ -7,7 +7,7 @@ import com.macro.mall.model.PmsSkuStock;
 import java.util.List;
 
 /**
- * 购物车中选择规格的商品信息
+ * 购物车中带规格和SKU的商品信息
  * Created by macro on 2018/8/2.
  */
 public class CartProduct extends PmsProduct {
