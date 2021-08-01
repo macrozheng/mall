0c728d4bb6e06d02dabb916f96a1f6c0ababa492
macro <zhenghong1991@126.com>
2021-08-01 16:28:36 +0800

    Update OmsOrderDetail.java

--- mall-portal/src/main/java/com/macro/mall/portal/domain/OmsOrderDetail.java
+++ mall-portal/src/main/java/com/macro/mall/portal/domain/OmsOrderDetail.java
@@ -6,7 +6,7 @@ import com.macro.mall.model.OmsOrderItem;
 import java.util.List;
 
 /**
- * 包含订单商品信息的订单详情
+ * 包含商品信息的订单详情
  * Created by macro on 2018/9/4.
  */
 public class OmsOrderDetail extends OmsOrder {
