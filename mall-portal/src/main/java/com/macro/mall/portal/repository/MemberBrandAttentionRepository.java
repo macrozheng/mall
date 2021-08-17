326006f9942a398cc20fcbeddcf41e6aa792fff1
macro <zhenghong1991@126.com>
2021-08-17 20:41:26 +0800

    Update MemberBrandAttentionRepository.java

--- mall-portal/src/main/java/com/macro/mall/portal/repository/MemberBrandAttentionRepository.java
+++ mall-portal/src/main/java/com/macro/mall/portal/repository/MemberBrandAttentionRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 /**
- * 会员关注Repository
+ * 会员品牌关注Repository
  * Created by macro on 2018/8/2.
  */
 public interface MemberBrandAttentionRepository extends MongoRepository<MemberBrandAttention,String> {
