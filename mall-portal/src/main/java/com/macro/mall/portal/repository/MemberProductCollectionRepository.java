0e22b919c4d4ec47625d760b86f53e1cfbe911de
macro <zhenghong1991@126.com>
2021-08-19 20:35:35 +0800

    Update MemberProductCollectionRepository.java

--- mall-portal/src/main/java/com/macro/mall/portal/repository/MemberProductCollectionRepository.java
+++ mall-portal/src/main/java/com/macro/mall/portal/repository/MemberProductCollectionRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 /**
- * 商品收藏Repository
+ * 会员商品收藏Repository
  * Created by macro on 2018/8/2.
  */
 public interface MemberProductCollectionRepository extends MongoRepository<MemberProductCollection,String> {
