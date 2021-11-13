a6caa90128ae509e877a4b1469c40f3fd0fa4b71
macro <zhenghong1991@126.com>
2021-11-13 10:17:23 +0800

    Update SwaggerConfig.java

--- mall-demo/src/main/java/com/macro/mall/demo/config/SwaggerConfig.java
+++ mall-demo/src/main/java/com/macro/mall/demo/config/SwaggerConfig.java
@@ -7,7 +7,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Swagger API文档相关配置
- * Created by macro on 2019/4/8.
+ * Created by macro on 2018/4/26.
  */
 @Configuration
 @EnableSwagger2
