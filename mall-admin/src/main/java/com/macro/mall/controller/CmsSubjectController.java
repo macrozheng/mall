f2b823322e630794f61f3206430e12f01a359740
macro <zhenghong1991@126.com>
2021-11-16 10:11:46 +0800

    Update CmsSubjectController.java

--- mall-admin/src/main/java/com/macro/mall/controller/CmsSubjectController.java
+++ mall-admin/src/main/java/com/macro/mall/controller/CmsSubjectController.java
@@ -34,7 +34,7 @@ public class CmsSubjectController {
         return CommonResult.success(subjectList);
     }
 
-    @ApiOperation(value = "根据专题名称分页获取专题")
+    @ApiOperation(value = "根据专题名称分页获取商品专题")
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     @ResponseBody
     public CommonResult<CommonPage<CmsSubject>> getList(@RequestParam(value = "keyword", required = false) String keyword,
