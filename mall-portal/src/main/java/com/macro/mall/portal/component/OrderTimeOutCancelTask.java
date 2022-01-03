663e2d706a287b53dd95abfd0da80e40062f51a6
macro <zhenghong1991@126.com>
2022-01-03 15:40:13 +0800

    Update OrderTimeOutCancelTask.java

--- mall-portal/src/main/java/com/macro/mall/portal/component/OrderTimeOutCancelTask.java
+++ mall-portal/src/main/java/com/macro/mall/portal/component/OrderTimeOutCancelTask.java
@@ -19,7 +19,7 @@ public class OrderTimeOutCancelTask {
 
     /**
      * cron表达式：Seconds Minutes Hours DayofMonth Month DayofWeek [Year]
-     * 每10分钟扫描一次，扫描设定超时时间之前下的订单，如果没支付则取消该订单
+     * 每10分钟扫描一次，扫描超时未支付订单，进行取消操作
      */
     @Scheduled(cron = "0 0/10 * ? * ?")
     private void cancelTimeOutOrder(){
