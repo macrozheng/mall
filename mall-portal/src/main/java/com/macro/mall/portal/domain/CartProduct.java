c075e116b590b784a5d825655c5a6a8708649e22
macro <zhenghong1991@126.com>
2022-02-07 15:32:06 +0800

    Update CartProduct.java

--- mall-portal/src/main/java/com/macro/mall/portal/domain/CartProduct.java
+++ mall-portal/src/main/java/com/macro/mall/portal/domain/CartProduct.java
@@ -3,6 +3,11 @@ package com.macro.mall.portal.domain;
 import com.macro.mall.model.PmsProduct;
 import com.macro.mall.model.PmsProductAttribute;
 import com.macro.mall.model.PmsSkuStock;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
@@ -10,25 +15,11 @@ import java.util.List;
  * 购物车中带规格和SKU的商品信息
  * Created by macro on 2018/8/2.
  */
+@Getter
+@Setter
 public class CartProduct extends PmsProduct {
-    //商品属性列表
+    @ApiModelProperty("商品属性列表")
     private List<PmsProductAttribute> productAttributeList;
-    //商品SKU库存列表
+    @ApiModelProperty("商品SKU库存列表")
     private List<PmsSkuStock> skuStockList;
-
-    public List<PmsProductAttribute> getProductAttributeList() {
-        return productAttributeList;
-    }
-
-    public void setProductAttributeList(List<PmsProductAttribute> productAttributeList) {
-        this.productAttributeList = productAttributeList;
-    }
-
-    public List<PmsSkuStock> getSkuStockList() {
-        return skuStockList;
-    }
-
-    public void setSkuStockList(List<PmsSkuStock> skuStockList) {
-        this.skuStockList = skuStockList;
-    }
 }
