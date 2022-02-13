9ce31043c94f2674e0021e86686eca10afbb0333
macro <zhenghong1991@126.com>
2022-02-13 19:35:49 +0800

    Update FlashPromotionProduct.java

--- mall-portal/src/main/java/com/macro/mall/portal/domain/FlashPromotionProduct.java
+++ mall-portal/src/main/java/com/macro/mall/portal/domain/FlashPromotionProduct.java
@@ -1,6 +1,7 @@
 package com.macro.mall.portal.domain;
 
 import com.macro.mall.model.PmsProduct;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,10 +14,10 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 public class FlashPromotionProduct extends PmsProduct{
-    //秒杀价格
+    @ApiModelProperty("秒杀价格")
     private BigDecimal flashPromotionPrice;
-    //用于秒杀到数量
+    @ApiModelProperty("用于秒杀到数量")
     private Integer flashPromotionCount;
-    //秒杀限购数量
+    @ApiModelProperty("秒杀限购数量")
     private Integer flashPromotionLimit;
 }
