830d00887c73deaad9d7315cb3254d18fe72403d
macro <zhenghong1991@126.com>
2022-02-23 20:53:53 +0800

    Update OmsOrderDetail.java

--- mall-portal/src/main/java/com/macro/mall/portal/domain/OmsOrderDetail.java
+++ mall-portal/src/main/java/com/macro/mall/portal/domain/OmsOrderDetail.java
@@ -2,6 +2,9 @@ package com.macro.mall.portal.domain;
 
 import com.macro.mall.model.OmsOrder;
 import com.macro.mall.model.OmsOrderItem;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
@@ -9,15 +12,9 @@ import java.util.List;
  * 包含商品信息的订单详情
  * Created by macro on 2018/9/4.
  */
+@Getter
+@Setter
 public class OmsOrderDetail extends OmsOrder {
-    //订单商品列表
+    @ApiModelProperty("订单商品列表")
     private List<OmsOrderItem> orderItemList;
-
-    public List<OmsOrderItem> getOrderItemList() {
-        return orderItemList;
-    }
-
-    public void setOrderItemList(List<OmsOrderItem> orderItemList) {
-        this.orderItemList = orderItemList;
-    }
 }
