f6bf065b0ed0dcaab316d2c7378b0f1df326251f
macro <zhenghong1991@126.com>
2022-03-04 19:36:20 +0800

    Update PromotionProduct.java

--- mall-portal/src/main/java/com/macro/mall/portal/domain/PromotionProduct.java
+++ mall-portal/src/main/java/com/macro/mall/portal/domain/PromotionProduct.java
@@ -4,6 +4,8 @@ import com.macro.mall.model.PmsProduct;
 import com.macro.mall.model.PmsProductFullReduction;
 import com.macro.mall.model.PmsProductLadder;
 import com.macro.mall.model.PmsSkuStock;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
@@ -11,6 +13,8 @@ import java.util.List;
  * 促销商品信息，包括sku、打折优惠、满减优惠
  * Created by macro on 2018/8/27.
  */
+@Getter
+@Setter
 public class PromotionProduct extends PmsProduct {
     //商品库存信息
     private List<PmsSkuStock> skuStockList;
@@ -18,28 +22,4 @@ public class PromotionProduct extends PmsProduct {
     private List<PmsProductLadder> productLadderList;
     //商品满减信息
     private List<PmsProductFullReduction> productFullReductionList;
-
-    public List<PmsSkuStock> getSkuStockList() {
-        return skuStockList;
-    }
-
-    public void setSkuStockList(List<PmsSkuStock> skuStockList) {
-        this.skuStockList = skuStockList;
-    }
-
-    public List<PmsProductLadder> getProductLadderList() {
-        return productLadderList;
-    }
-
-    public void setProductLadderList(List<PmsProductLadder> productLadderList) {
-        this.productLadderList = productLadderList;
-    }
-
-    public List<PmsProductFullReduction> getProductFullReductionList() {
-        return productFullReductionList;
-    }
-
-    public void setProductFullReductionList(List<PmsProductFullReduction> productFullReductionList) {
-        this.productFullReductionList = productFullReductionList;
-    }
 }
