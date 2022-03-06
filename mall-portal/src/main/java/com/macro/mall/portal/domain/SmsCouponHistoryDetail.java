e5811f5c0fb0377452c22e9109f88e6ec77823c2
macro <zhenghong1991@126.com>
2022-03-06 16:00:43 +0800

    Update SmsCouponHistoryDetail.java

--- mall-portal/src/main/java/com/macro/mall/portal/domain/SmsCouponHistoryDetail.java
+++ mall-portal/src/main/java/com/macro/mall/portal/domain/SmsCouponHistoryDetail.java
@@ -4,6 +4,9 @@ import com.macro.mall.model.SmsCoupon;
 import com.macro.mall.model.SmsCouponHistory;
 import com.macro.mall.model.SmsCouponProductCategoryRelation;
 import com.macro.mall.model.SmsCouponProductRelation;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
@@ -11,35 +14,13 @@ import java.util.List;
  * 优惠券领取历史详情（包括优惠券信息和关联关系）
  * Created by macro on 2018/8/29.
  */
+@Getter
+@Setter
 public class SmsCouponHistoryDetail extends SmsCouponHistory {
-    //相关优惠券信息
+    @ApiModelProperty("相关优惠券信息")
     private SmsCoupon coupon;
-    //优惠券关联商品
+    @ApiModelProperty("优惠券关联商品")
     private List<SmsCouponProductRelation> productRelationList;
-    //优惠券关联商品分类
+    @ApiModelProperty("优惠券关联商品分类")
     private List<SmsCouponProductCategoryRelation> categoryRelationList;
-
-    public SmsCoupon getCoupon() {
-        return coupon;
-    }
-
-    public void setCoupon(SmsCoupon coupon) {
-        this.coupon = coupon;
-    }
-
-    public List<SmsCouponProductRelation> getProductRelationList() {
-        return productRelationList;
-    }
-
-    public void setProductRelationList(List<SmsCouponProductRelation> productRelationList) {
-        this.productRelationList = productRelationList;
-    }
-
-    public List<SmsCouponProductCategoryRelation> getCategoryRelationList() {
-        return categoryRelationList;
-    }
-
-    public void setCategoryRelationList(List<SmsCouponProductCategoryRelation> categoryRelationList) {
-        this.categoryRelationList = categoryRelationList;
-    }
 }
