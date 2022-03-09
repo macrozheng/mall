e8ac377d0bdb9cf42c5d28f92a47aa387423c1a5
macro <zhenghong1991@126.com>
2022-03-09 10:03:50 +0800

    Update MemberBrandAttention.java

--- mall-portal/src/main/java/com/macro/mall/portal/domain/MemberBrandAttention.java
+++ mall-portal/src/main/java/com/macro/mall/portal/domain/MemberBrandAttention.java
@@ -1,5 +1,7 @@
 package com.macro.mall.portal.domain;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,6 +12,8 @@ import java.util.Date;
  * 会员品牌关注
  * Created by macro on 2018/8/2.
  */
+@Getter
+@Setter
 @Document
 public class MemberBrandAttention {
     @Id
@@ -24,76 +28,4 @@ public class MemberBrandAttention {
     private String brandLogo;
     private String brandCity;
     private Date createTime;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Long getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
-
-    public String getMemberNickname() {
-        return memberNickname;
-    }
-
-    public void setMemberNickname(String memberNickname) {
-        this.memberNickname = memberNickname;
-    }
-
-    public String getMemberIcon() {
-        return memberIcon;
-    }
-
-    public void setMemberIcon(String memberIcon) {
-        this.memberIcon = memberIcon;
-    }
-
-    public Long getBrandId() {
-        return brandId;
-    }
-
-    public void setBrandId(Long brandId) {
-        this.brandId = brandId;
-    }
-
-    public String getBrandName() {
-        return brandName;
-    }
-
-    public void setBrandName(String brandName) {
-        this.brandName = brandName;
-    }
-
-    public String getBrandLogo() {
-        return brandLogo;
-    }
-
-    public void setBrandLogo(String brandLogo) {
-        this.brandLogo = brandLogo;
-    }
-
-    public String getBrandCity() {
-        return brandCity;
-    }
-
-    public void setBrandCity(String brandCity) {
-        this.brandCity = brandCity;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
 }
