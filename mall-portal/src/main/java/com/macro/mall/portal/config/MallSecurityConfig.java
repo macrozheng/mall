aaed33f58795bf8c597cb363f92563a9d9c6bd30
macro <zhenghong1991@126.com>
2022-03-26 10:07:01 +0800

    Update MallSecurityConfig.java

--- mall-portal/src/main/java/com/macro/mall/portal/config/MallSecurityConfig.java
+++ mall-portal/src/main/java/com/macro/mall/portal/config/MallSecurityConfig.java
@@ -1,19 +1,13 @@
 package com.macro.mall.portal.config;
 
 import com.macro.mall.portal.service.UmsMemberService;
-import com.macro.mall.security.component.DynamicSecurityService;
 import com.macro.mall.security.config.SecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-
-import java.util.Collection;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * mall-security模块相关配置
