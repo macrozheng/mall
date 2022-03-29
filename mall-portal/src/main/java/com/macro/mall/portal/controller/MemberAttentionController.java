b7e6b6cf7ce5c1facb9d09d8f6b17c530aecc151
macro <zhenghong1991@126.com>
2022-03-29 17:38:22 +0800

    Update MemberAttentionController.java

--- mall-portal/src/main/java/com/macro/mall/portal/controller/MemberAttentionController.java
+++ mall-portal/src/main/java/com/macro/mall/portal/controller/MemberAttentionController.java
@@ -3,7 +3,6 @@ package com.macro.mall.portal.controller;
 import com.macro.mall.common.api.CommonPage;
 import com.macro.mall.common.api.CommonResult;
 import com.macro.mall.portal.domain.MemberBrandAttention;
-import com.macro.mall.portal.domain.MemberProductCollection;
 import com.macro.mall.portal.service.MemberAttentionService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -11,8 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 /**
  * 会员关注品牌管理Controller
