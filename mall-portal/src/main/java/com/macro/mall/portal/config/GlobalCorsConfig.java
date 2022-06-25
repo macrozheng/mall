d59951511a819168f5d475cab53d3f0dbeee2245
macro <zhenghong1991@126.com>
2022-06-25 20:18:47 +0800

    跨域问题解决

--- mall-portal/src/main/java/com/macro/mall/portal/config/GlobalCorsConfig.java
+++ mall-portal/src/main/java/com/macro/mall/portal/config/GlobalCorsConfig.java
@@ -20,7 +20,7 @@ public class GlobalCorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         //允许所有域名进行跨域调用
-        config.addAllowedOrigin("*");
+        config.addAllowedOriginPattern("*");
         //允许跨越发送cookie
         config.setAllowCredentials(true);
         //放行全部原始头信息
