9b126c77e5784edcb6e01f8651b07dc1b6124aa7
howe <834539153@qq.com>
2022-08-04 17:39:37 +0800

    test

--- mall-common/src/main/java/com/macro/mall/common/service/impl/RedisServiceImpl.java
+++ mall-common/src/main/java/com/macro/mall/common/service/impl/RedisServiceImpl.java
@@ -20,6 +20,7 @@ public class RedisServiceImpl implements RedisService {
     @Override
     public void set(String key, Object value, long time) {
         redisTemplate.opsForValue().set(key, value, time, TimeUnit.SECONDS);
+
     }
 
     @Override
