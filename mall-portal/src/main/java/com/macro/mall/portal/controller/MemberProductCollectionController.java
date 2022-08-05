27c7606179ea3072baea8970e00128c29828c078
macro <zhenghong1991@126.com>
2022-08-05 20:38:24 +0800

    Update MemberProductCollectionController.java

--- mall-portal/src/main/java/com/macro/mall/portal/controller/MemberProductCollectionController.java
+++ mall-portal/src/main/java/com/macro/mall/portal/controller/MemberProductCollectionController.java
@@ -6,6 +6,7 @@ import com.macro.mall.portal.domain.MemberProductCollection;
 import com.macro.mall.portal.service.MemberCollectionService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
@@ -16,7 +17,8 @@ import org.springframework.web.bind.annotation.*;
  * Created by macro on 2018/8/2.
  */
 @Controller
-@Api(tags = "MemberCollectionController", description = "会员收藏管理")
+@Api(tags = "MemberCollectionController")
+@Tag(name = "MemberCollectionController",description = "会员收藏管理")
 @RequestMapping("/member/productCollection")
 public class MemberProductCollectionController {
     @Autowired
