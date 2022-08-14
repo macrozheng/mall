4d6c877a6a98b16af607573c317f800ea3f5fb82
macro <zhenghong1991@126.com>
2022-08-14 15:05:11 +0800

    Update UmsMemberReceiveAddressController.java

--- mall-portal/src/main/java/com/macro/mall/portal/controller/UmsMemberReceiveAddressController.java
+++ mall-portal/src/main/java/com/macro/mall/portal/controller/UmsMemberReceiveAddressController.java
@@ -5,6 +5,7 @@ import com.macro.mall.model.UmsMemberReceiveAddress;
 import com.macro.mall.portal.service.UmsMemberReceiveAddressService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +17,8 @@ import java.util.List;
  * Created by macro on 2018/8/28.
  */
 @Controller
-@Api(tags = "UmsMemberReceiveAddressController", description = "会员收货地址管理")
+@Api(tags = "UmsMemberReceiveAddressController")
+@Tag(name = "UmsMemberReceiveAddressController", description = "会员收货地址管理")
 @RequestMapping("/member/address")
 public class UmsMemberReceiveAddressController {
     @Autowired
