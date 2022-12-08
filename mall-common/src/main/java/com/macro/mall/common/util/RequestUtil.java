68f335b31fa7bc4edd531298110764bd87731cfb
zhuqinshu <**zhuqinshu@kezaihui.com**>
2022-12-08 18:08:26 +0800

    请求的工具类的魔法值问题和判空的优化

--- mall-common/src/main/java/com/macro/mall/common/util/RequestUtil.java
+++ mall-common/src/main/java/com/macro/mall/common/util/RequestUtil.java
@@ -1,5 +1,7 @@
 package com.macro.mall.common.util;
 
+import cn.hutool.core.util.StrUtil;
+
 import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -10,22 +12,38 @@ import java.net.UnknownHostException;
  */
 public class RequestUtil {
 
+
+    private static final String UNKNOWN = "UNKNOWN";
+
+    private static final String HEAD_FORWARDED = "x-forwarded-for";
+
+    private static final String PROXY_CLIENT_IP = "Proxy-Client-IP";
+
+    private static final String WL_PROXY_CLIENT_IP = "WL-Proxy-Client-IP";
+
+    private static final String LOCALHOST_IP_A = "127.0.0.1";
+
+    private static final String LOCALHOST_IP_B = "0:0:0:0:0:0:0:1";
+
+    private static final int IP_LIMIT_NUM = 15;
+
+
     /**
      * 获取请求真实IP地址
      */
     public static String getRequestIp(HttpServletRequest request) {
         //通过HTTP代理服务器转发时添加
-        String ipAddress = request.getHeader("x-forwarded-for");
-        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
-            ipAddress = request.getHeader("Proxy-Client-IP");
+        String ipAddress = request.getHeader(HEAD_FORWARDED);
+        if (StrUtil.isBlank(ipAddress) || UNKNOWN.equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader(PROXY_CLIENT_IP);
         }
-        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
-            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        if (StrUtil.isBlank(ipAddress) || UNKNOWN.equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader(WL_PROXY_CLIENT_IP);
         }
-        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+        if (StrUtil.isBlank(ipAddress)  || UNKNOWN.equalsIgnoreCase(ipAddress)) {
             ipAddress = request.getRemoteAddr();
             // 从本地访问时根据网卡取本机配置的IP
-            if (ipAddress.equals("127.0.0.1") || ipAddress.equals("0:0:0:0:0:0:0:1")) {
+            if (ipAddress.equals(LOCALHOST_IP_A) || ipAddress.equals(LOCALHOST_IP_B)) {
                 InetAddress inetAddress = null;
                 try {
                     inetAddress = InetAddress.getLocalHost();
@@ -36,9 +54,9 @@ public class RequestUtil {
             }
         }
         // 通过多个代理转发的情况，第一个IP为客户端真实IP，多个IP会按照','分割
-        if (ipAddress != null && ipAddress.length() > 15) {
-            if (ipAddress.indexOf(",") > 0) {
-                ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+        if (ipAddress != null && ipAddress.length() > IP_LIMIT_NUM) {
+            if (ipAddress.indexOf(StrUtil.COMMA) > 0) {
+                ipAddress = ipAddress.substring(0, ipAddress.indexOf(StrUtil.COMMA));
             }
         }
         return ipAddress;
