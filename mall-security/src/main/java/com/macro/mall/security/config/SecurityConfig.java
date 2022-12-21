fed8f5b02c343e31ba437b7a3eb177e8c3ef9bec
macro <zhenghong1991@126.com>
2022-12-21 17:06:39 +0800

    Update SecurityConfig.java

--- mall-security/src/main/java/com/macro/mall/security/config/SecurityConfig.java
+++ mall-security/src/main/java/com/macro/mall/security/config/SecurityConfig.java
@@ -22,7 +22,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 
 /**
- * SpringSecurity 5.4.x以上新用法配置，仅用于配置HttpSecurity
+ * SpringSecurity相关配置，仅用于配置SecurityFilterChain
  * Created by macro on 2019/11/5.
  */
 @Configuration
