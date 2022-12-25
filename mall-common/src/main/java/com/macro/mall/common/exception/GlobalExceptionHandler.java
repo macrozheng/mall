13b7be0bdfb5c899a9dbb0eb336d64cb933e538b
macro <zhenghong1991@126.com>
2022-12-25 16:20:00 +0800

    Update GlobalExceptionHandler.java

--- mall-common/src/main/java/com/macro/mall/common/exception/GlobalExceptionHandler.java
+++ mall-common/src/main/java/com/macro/mall/common/exception/GlobalExceptionHandler.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * 全局异常处理
+ * 全局异常处理类
  * Created by macro on 2020/2/27.
  */
 @ControllerAdvice
