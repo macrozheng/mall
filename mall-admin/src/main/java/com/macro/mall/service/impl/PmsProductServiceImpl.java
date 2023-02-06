3c7f48d0c27fc3c88f9743778f6cff500e8c5fc3
macro <zhenghong1991@126.com>
2023-02-06 09:38:44 +0800

    Update PmsProductServiceImpl.java

--- mall-admin/src/main/java/com/macro/mall/service/impl/PmsProductServiceImpl.java
+++ mall-admin/src/main/java/com/macro/mall/service/impl/PmsProductServiceImpl.java
@@ -15,7 +15,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
-import org.springframework.util.StringUtils;
 
 import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
