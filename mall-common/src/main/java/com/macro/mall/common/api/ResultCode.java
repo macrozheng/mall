19ebdadf19558b80ad1fc6ddcee1a6079b8106d2
Daven Dus <dusdaven@gmail.com>
2023-09-09 00:18:15 +0800

    parater checking error should be 400, 404 mens url not found

--- mall-common/src/main/java/com/macro/mall/common/api/ResultCode.java
+++ mall-common/src/main/java/com/macro/mall/common/api/ResultCode.java
@@ -7,7 +7,7 @@ package com.macro.mall.common.api;
 public enum ResultCode implements IErrorCode {
     SUCCESS(200, "操作成功"),
     FAILED(500, "操作失败"),
-    VALIDATE_FAILED(404, "参数检验失败"),
+    VALIDATE_FAILED(400, "参数检验失败"),
     UNAUTHORIZED(401, "暂未登录或token已经过期"),
     FORBIDDEN(403, "没有相关权限");
     private long code;
