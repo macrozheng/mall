a60dde48587dc16a57a00456ce6ce80d9b6b2b6c
Daven Dus <dusdaven@gmail.com>
2023-09-09 00:26:56 +0800

    测试

--- mall-common/src/main/java/com/macro/mall/common/api/ResultCode.java
+++ mall-common/src/main/java/com/macro/mall/common/api/ResultCode.java
@@ -10,6 +10,7 @@ public enum ResultCode implements IErrorCode {
     VALIDATE_FAILED(400, "参数检验失败"),
     UNAUTHORIZED(401, "暂未登录或token已经过期"),
     FORBIDDEN(403, "没有相关权限");
+
     private long code;
     private String message;
 
