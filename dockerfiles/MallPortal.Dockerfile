d3e763549992a6b7dbcc7d78e0f5942b41b1555f
wildeslam <yushuwang@trip.com>
2024-01-25 11:25:07 +0800

    eng

--- dockerfiles/MallPortal.Dockerfile
+++ dockerfiles/MallPortal.Dockerfile
@@ -1,4 +1,7 @@
 FROM openjdk:8
 ADD mall-portal-1.0-SNAPSHOT.jar /mall-portal-1.0-SNAPSHOT.jar
+ADD arex-agent.jar /usr/local/tomcat/
+ADD arex-agent-bootstrap.jar /usr/local/tomcat/
+ENV JAVA_TOOL_OPTIONS="-javaagent:/usr/local/tomcat/arex-agent.jar -Darex.service.name=Arex-Mall -Darex.storage.service.host=10.144.62.53:8093 -Darex.enable.debug=true"
 EXPOSE 8085
 ENTRYPOINT ["java", "-jar","/mall-portal-1.0-SNAPSHOT.jar"]
--- mall-common/src/main/java/com/macro/mall/common/api/ResultCode.java
+++ mall-common/src/main/java/com/macro/mall/common/api/ResultCode.java
@@ -5,11 +5,11 @@ package com.macro.mall.common.api;
  * Created by macro on 2019/4/19.
  */
 public enum ResultCode implements IErrorCode {
-    SUCCESS(200, "操作成功"),
-    FAILED(500, "操作失败"),
-    VALIDATE_FAILED(404, "参数检验失败"),
-    UNAUTHORIZED(401, "暂未登录或token已经过期"),
-    FORBIDDEN(403, "没有相关权限");
+    SUCCESS(200, "Success"),
+    FAILED(500, "Failed"),
+    VALIDATE_FAILED(404, "Validation failed"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    FORBIDDEN(403, "Forbidden");
     private long code;
     private String message;
 
